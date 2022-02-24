833e0463368ea391ed93f7d074c2c18ef7ef84f1
ayazeid <aya.zeid95@gmail.com>
2022-02-24 08:46:52 +0200

    Add doctor models to admin panel

--- backend/pets_treatment/users/admin.py
+++ backend/pets_treatment/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Doctor,Specialization,DoctorSpecialization
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Specialization)
+admin.site.register(DoctorSpecialization)
