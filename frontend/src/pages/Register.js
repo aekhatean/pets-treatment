08a63fe5d85046e185eff56b8e5bf6bcb45cdcdc
AyaMohamed1 <yoyomohamed102000@gmail.com>
2022-03-02 17:16:05 +0200

    added drop down for city and area

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -28,8 +28,6 @@ function Register() {
         .max(14, "National id acn't be more than 14 character")
         .required("National id is required"),
 
-
-        // edit phone num with regex
         phone:Yup.string()
         .max(11, "Eqyptian num")
         .required("Phone is required")
@@ -38,17 +36,32 @@ function Register() {
             "Must be egyptian number"
           ),
 
-        // edit next 3 to be drop down
-        // country:Yup.string()
-        // .required("Country is required"),
-        city:Yup.string()
-        .required("City is required"),
-        area:Yup.string()
-        .required("Area is required"),
-
 
 
     })
+
+    const getareas = city => {
+        // Simulate async call
+        return new Promise((resolve, reject) => {
+          switch (city) {
+            case "Giza":
+              resolve([
+                {value: "dokki", label: "Dokki"},
+                {value: "6th_october", label:"6th October"},
+              ]);
+              break;
+            case "Cairo":
+              resolve([
+                {value: "nasr_city", label: "Nasr City"},
+                {value: "shoubra", label:"Shoubra"},
+              ]);
+              break;
+            default:
+              resolve([]);
+          }
+        });
+      };
+
     return (
         <Formik
             initialValues={{
@@ -63,20 +76,33 @@ function Register() {
                 country: 'egypt',
                 city: '',
                 area: '',
+                areas:[],
                 syndicate_id: '',
                 photo: '',
 
 
             }}
             validationSchema={validate}
-            onSubmit={values =>{
-                console.log(values)
-            }}
+            onSubmit = {async values => {
+                await new Promise(resolve => setTimeout(resolve, 500));
+                alert(JSON.stringify(values, null, 2));
+              }}
+              
         >
-            {formProps => (
+        {formProps => {
+          const {
+            values,
+            notValid,
+            isSubmitting,
+            handleChange,
+            handleSubmit,
+            handleReset,
+            setFieldValue
+          } = formProps;
+          return (
                 <div>
                     <h1 className='my-4 font-weight-bold-display-4'>Register as a Doctor</h1>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <TextFeild label="First Name" name="firstName" type="text"/>
                         <TextFeild label="Last Name" name="lastName" type="text"/>
                         <TextFeild label="Username" name="username" type="text"/>
@@ -91,7 +117,8 @@ function Register() {
                         <label for="doc_photo">Upload your Photo</label>
                         <Input type="file" id="doc_photo" name="photo" onChange={(event) => formProps.setFieldValue('photo', event.target.files[0])}/>
                         <label for="synd_id">Upload your Syndicate id</label>
-                        <Input type="file" id="synd_id" name="syndicate_id" onChange={(event) => formProps.setFieldValue('syndicate_id', event.target.files[0])}/>
+                        <Input type="file" id="synd_id" name="syndicate_id" 
+                        onChange={(event) => formProps.setFieldValue('syndicate_id', event.target.files[0])}/>
                         
 
                         <label for="country">Country</label>
@@ -100,19 +127,52 @@ function Register() {
                         </Field>
 
 
+                        <label htmlFor="city">city</label>
+              <Field
+                id="city"
+                name="city"
+                as="select"
+                value={values.city}
+                onChange={async e => {
+                  const { value } = e.target;
+                  const _areas = await getareas(value);
+                  console.log(_areas);
+                  setFieldValue("city", value);
+                  setFieldValue("area", "");
+                  setFieldValue("areas", _areas);
+                }}
+              >
+                <option value="None">Select city</option>
+                <option value="Giza">Giza</option>
+                <option value="Cairo">Cairo</option>
+              </Field>
 
+              <label htmlFor="area">area</label>
+              <Field
+                value={values.area}
+                id="area"
+                name="area"
+                as="select"
+                onChange={handleChange}
+              >
+                <option value="None">Select area</option>
+                {values.areas &&
+                  values.areas.map(a => (
+                    <option key={a.value} value={a.value}>
+                      {a.label}
+                    </option>
+                  ))}
+              </Field>
             
-
-                        <TextFeild label="City" name="city" type="text"/>
-                        <TextFeild label="Area" name="area" type="text"/>
                         
                         
                         
-                        <button className='btn mt-3 btn-dark' type='submit'>Submit</button>
-                        <button className='btn mt-3 ml-3 btn-danger' type='reset'>Reset</button>
+                        <button className='btn mt-3 btn-dark' type='submit' disabled={isSubmitting} >Submit</button>
+                        <button className='btn mt-3 ml-3 btn-danger' type='reset' onClick={handleReset}
+                disabled={!notValid || isSubmitting}>Reset</button>
                     </Form>
                 </div>
-            )}
+            )}}
         </Formik>
     )
 
