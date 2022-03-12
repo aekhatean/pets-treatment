59b898793787650f918702d216b59e6deb60ffc5
ayazeid <aya.zeid95@gmail.com>
2022-03-12 15:34:47 +0200

    Fix routing for petowner register

--- frontend/src/pages/PetOwnerRegister.js
+++ frontend/src/pages/PetOwnerRegister.js
@@ -6,6 +6,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 import { colors } from "../colors/colors";
 import { LanguageContext } from "../context/LanguageContext";
+import { LogingContext } from "../context/LogingContext";
 import { content } from "../translation/translation";
 import { useHistory, Redirect } from "react-router-dom";
 import ModalSuccess from "../components/ModalSuccess";
@@ -27,6 +28,8 @@ function Register() {
   const { lang, setLang } = useContext(LanguageContext);
   const [isModalSuccessOpen, setIsModalSuccessOpen] = useState(false);
   const [isModalFailOpen, setIsModalFailOpen] = useState(false);
+  const { is_loged, setLogging } = useContext(LogingContext);
+
   const validate = Yup.object({
     firstName: Yup.string()
       .max(15, content[lang].invalid_firstname)
@@ -89,229 +92,249 @@ function Register() {
       }
     });
   };
+  if (is_loged) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <>
-    <img src={login_cat} alt="catty"/>
-    <Formik
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-        phone: "",
-        country: "egypt",
-        city: "",
-        area: "",
-        areas: [],
-        photo: null,
-        username: "",
-      }}
-      validationSchema={validate}
-      onSubmit={(values) => {
-        const data = {
-          user: {
-            first_name: values.firstName,
-            last_name: values.lastName,
-            password: values.password,
-            email: values.email,
-            username: values.username,
-          },
-          country: values.country,
-          city: values.city,
-          area: values.area,
-          phone: values.phone,
-          picture: values.photo,
-        };
-        axios
-          .post("http://127.0.0.1:8000/users/register/", data)
-          .then((response) => {
-            console.log(response);
-            setIsModalSuccessOpen(true);
-          })
-          .catch((e) => {
-            console.log(e.response);
-            setIsModalFailOpen(true);
-          });
-      }}
-    >
-      {(formProps) => {
-        const {
-          values,
-          handleChange,
-          handleSubmit,
-          handleReset,
-          setFieldValue,
-        } = formProps;
-        return (
-          <Container className="p-5 mb-5 shadow" dir={lang === 'ar' ? 'rtl' : 'ltr'} style={{width:'50%', borderRadius:20}}>
-          <span className="center">
-          
-          <h1 className=" font-weight-bold-display-4" style={{display: "inline"}}>
-              {content[lang].register_petowner}
-          </h1>
-          
-        </span>
-            <Form onSubmit={handleSubmit}>
-              <TextFeild
-                label={content[lang].fist_name}
-                name="firstName"
-                type="text"
-              />
-              <TextFeild
-                label={content[lang].last_name}
-                name="lastName"
-                type="text"
-              />
-              <TextFeild
-                label={content[lang].username}
-                name="username"
-                type="text"
-              />
-              <TextFeild
-                label={content[lang].email}
-                name="email"
-                type="email"
-              />
-              <TextFeild
-                label={content[lang].password}
-                name="password"
-                type="password"
-              />
-              <TextFeild
-                label={content[lang].confirm_password}
-                name="confirmPassword"
-                type="password"
-              />
-              <TextFeild label={content[lang].phone} name="phone" type="text" />
-              <div
-                className={lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"}
-              >
-                <Field
-                  name="photo"
-                  component={FileUpload}
-                  label={content[lang].upload_photo}
-                  isCardStyles={false}
-                />
-              </div>
-              <div
-                className={lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"}
-              >
-                <label className="form-label" htmlFor="country">
-                  {content[lang].country}
-                </label>
-                <Field
-                  as="select"
-                  name="country"
-                  id="country"
-                  className="form-select"
+      <img src={login_cat} alt="catty" />
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+          phone: "",
+          country: "egypt",
+          city: "",
+          area: "",
+          areas: [],
+          photo: null,
+          username: "",
+        }}
+        validationSchema={validate}
+        onSubmit={(values) => {
+          const data = {
+            user: {
+              first_name: values.firstName,
+              last_name: values.lastName,
+              password: values.password,
+              email: values.email,
+              username: values.username,
+            },
+            country: values.country,
+            city: values.city,
+            area: values.area,
+            phone: values.phone,
+            picture: values.photo,
+          };
+          axios
+            .post("http://127.0.0.1:8000/users/register/", data)
+            .then((response) => {
+              console.log(response);
+              setIsModalSuccessOpen(true);
+            })
+            .catch((e) => {
+              console.log(e.response);
+              setIsModalFailOpen(true);
+            });
+        }}
+      >
+        {(formProps) => {
+          const {
+            values,
+            handleChange,
+            handleSubmit,
+            handleReset,
+            setFieldValue,
+          } = formProps;
+          return (
+            <Container
+              className="p-5 mb-5 shadow"
+              dir={lang === "ar" ? "rtl" : "ltr"}
+              style={{ width: "50%", borderRadius: 20 }}
+            >
+              <span className="center">
+                <h1
+                  className=" font-weight-bold-display-4"
+                  style={{ display: "inline" }}
                 >
-                  <option value="egypt">{content[lang].egypt}</option>
-                </Field>
-                <ErrorMessage
-                  name={"country"}
-                  component="div"
-                  style={{ color: "red" }}
-                  className="error"
+                  {content[lang].register_petowner}
+                </h1>
+              </span>
+              <Form onSubmit={handleSubmit}>
+                <TextFeild
+                  label={content[lang].fist_name}
+                  name="firstName"
+                  type="text"
                 />
-              </div>
-              <div
-                className={lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"}
-              >
-                <label className="form-label" htmlFor="city">
-                  {content[lang].city}
-                </label>
-                <Field
-                  id="city"
-                  name="city"
-                  as="select"
-                  className="form-select"
-                  controlId="validationFormik05"
-                  value={values.city}
-                  onChange={async (e) => {
-                    const { value } = e.target;
-                    const _areas = await getareas(value);
-                    setFieldValue("city", value);
-                    setFieldValue("area", "");
-                    setFieldValue("areas", _areas);
+                <TextFeild
+                  label={content[lang].last_name}
+                  name="lastName"
+                  type="text"
+                />
+                <TextFeild
+                  label={content[lang].username}
+                  name="username"
+                  type="text"
+                />
+                <TextFeild
+                  label={content[lang].email}
+                  name="email"
+                  type="email"
+                />
+                <TextFeild
+                  label={content[lang].password}
+                  name="password"
+                  type="password"
+                />
+                <TextFeild
+                  label={content[lang].confirm_password}
+                  name="confirmPassword"
+                  type="password"
+                />
+                <TextFeild
+                  label={content[lang].phone}
+                  name="phone"
+                  type="text"
+                />
+                <div
+                  className={
+                    lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"
+                  }
+                >
+                  <Field
+                    name="photo"
+                    component={FileUpload}
+                    label={content[lang].upload_photo}
+                    isCardStyles={false}
+                  />
+                </div>
+                <div
+                  className={
+                    lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"
+                  }
+                >
+                  <label className="form-label" htmlFor="country">
+                    {content[lang].country}
+                  </label>
+                  <Field
+                    as="select"
+                    name="country"
+                    id="country"
+                    className="form-select"
+                  >
+                    <option value="egypt">{content[lang].egypt}</option>
+                  </Field>
+                  <ErrorMessage
+                    name={"country"}
+                    component="div"
+                    style={{ color: "red" }}
+                    className="error"
+                  />
+                </div>
+                <div
+                  className={
+                    lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"
+                  }
+                >
+                  <label className="form-label" htmlFor="city">
+                    {content[lang].city}
+                  </label>
+                  <Field
+                    id="city"
+                    name="city"
+                    as="select"
+                    className="form-select"
+                    controlId="validationFormik05"
+                    value={values.city}
+                    onChange={async (e) => {
+                      const { value } = e.target;
+                      const _areas = await getareas(value);
+                      setFieldValue("city", value);
+                      setFieldValue("area", "");
+                      setFieldValue("areas", _areas);
+                    }}
+                  >
+                    <option value="None">{content[lang].select_city}</option>
+                    <option value="Giza">{content[lang].giza}</option>
+                    <option value="Cairo">{content[lang].cairo}</option>
+                  </Field>
+                  <ErrorMessage
+                    name={"city"}
+                    component="div"
+                    style={{ color: "red" }}
+                    className="error"
+                  />
+                </div>
+                <div
+                  className={
+                    lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"
+                  }
+                >
+                  <label className="form-label" htmlFor="area">
+                    {content[lang].area}
+                  </label>
+                  <Field
+                    value={values.area}
+                    id="area"
+                    name="area"
+                    as="select"
+                    className="form-select"
+                    controlId="validationFormik05"
+                    onChange={handleChange}
+                  >
+                    <option value="None">{content[lang].select_area}</option>
+                    {values.areas &&
+                      values.areas.map((a) => (
+                        <option key={a.value} value={a.value}>
+                          {a.label}
+                        </option>
+                      ))}
+                  </Field>
+                  <ErrorMessage
+                    name={"area"}
+                    component="div"
+                    style={{ color: "red" }}
+                    className="error"
+                  />
+                </div>
+                <button
+                  className="btn mt-3 mx-2 btn-outline-dark"
+                  type="submit"
+                  style={{
+                    marginRight: "10px",
+                    backgroundColor: colors.bg.primary,
+                    border: "none",
                   }}
                 >
-                  <option value="None">{content[lang].select_city}</option>
-                  <option value="Giza">{content[lang].giza}</option>
-                  <option value="Cairo">{content[lang].cairo}</option>
-                </Field>
-                <ErrorMessage
-                  name={"city"}
-                  component="div"
-                  style={{ color: "red" }}
-                  className="error"
-                />
-              </div>
-              <div
-                className={lang === "ar" ? "mb-3 text-end" : "mb-3 text-start"}
-              >
-                <label className="form-label" htmlFor="area">
-                  {content[lang].area}
-                </label>
-                <Field
-                  value={values.area}
-                  id="area"
-                  name="area"
-                  as="select"
-                  className="form-select"
-                  controlId="validationFormik05"
-                  onChange={handleChange}
+                  {content[lang].submit}
+                </button>
+                <button
+                  className="btn mt-3 mx-2 ml-3 btn-danger"
+                  type="reset"
+                  onClick={handleReset}
                 >
-                  <option value="None">{content[lang].select_area}</option>
-                  {values.areas &&
-                    values.areas.map((a) => (
-                      <option key={a.value} value={a.value}>
-                        {a.label}
-                      </option>
-                    ))}
-                </Field>
-                <ErrorMessage
-                  name={"area"}
-                  component="div"
-                  style={{ color: "red" }}
-                  className="error"
+                  {content[lang].reset}
+                </button>
+                <ModalSuccess
+                  setIsModalOpen={setIsModalSuccessOpen}
+                  isModalOpen={isModalSuccessOpen}
+                  successText={content[lang].verify_email}
+                  hideFunc={redic}
                 />
-              </div>
-              <button
-                className="btn mt-3 mx-2 btn-outline-dark"
-                type="submit"
-                style={{
-                  marginRight: "10px",
-                  backgroundColor: colors.bg.primary,
-                  border: "none",
-                }}
-              >
-                {content[lang].submit}
-              </button>
-              <button
-                className="btn mt-3 mx-2 ml-3 btn-danger"
-                type="reset"
-                onClick={handleReset}
-              >
-                {content[lang].reset}
-              </button>
-              <ModalSuccess
-                setIsModalOpen={setIsModalSuccessOpen}
-                isModalOpen={isModalSuccessOpen}
-                successText={content[lang].verify_email}
-                hideFunc={redic}
-              />
-              <ModalFail
-                setIsModalOpen={setIsModalFailOpen}
-                isModalOpen={isModalFailOpen}
-                errorText={content[lang].error_general_msg}
-              />
-            </Form>
-          </Container>
-        );
-      }}
-    </Formik>
+                <ModalFail
+                  setIsModalOpen={setIsModalFailOpen}
+                  isModalOpen={isModalFailOpen}
+                  errorText={content[lang].error_general_msg}
+                />
+              </Form>
+            </Container>
+          );
+        }}
+      </Formik>
     </>
   );
 }
